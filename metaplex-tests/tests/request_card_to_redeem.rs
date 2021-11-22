e89a57652c05423b4fea3be1420bf72f2c544d0e
bezrazli4n0 <rpglovemmo@gmail.com>
2021-11-22 19:12:31 +0300

    feature: added CleanUp positive tests, RequestCardToRedeem date & wrong voucher positive/negative tests, ClaimCard twice negative test, wip: RequestCardToRedeem all indexes positive test

--- metaplex-tests/tests/request_card_to_redeem.rs
+++ metaplex-tests/tests/request_card_to_redeem.rs
@@ -1,10 +1,12 @@
 mod utils;
 
 use metaplex_nft_packs::{
+    error::NFTPacksError,
     find_proving_process_program_address,
     instruction::{AddCardToPackArgs, InitPackSetArgs},
     state::{PackDistributionType, ProvingProcess},
 };
+use num_traits::FromPrimitive;
 use solana_program::{instruction::InstructionError, program_pack::Pack, system_instruction};
 use solana_program_test::*;
 use solana_sdk::{
@@ -480,6 +482,318 @@ async fn fail_request_without_clean_up() {
 
     assert_transport_error!(
         result.unwrap_err(),
+        TransportError::TransactionError(TransactionError::InstructionError(
+            0,
+            InstructionError::IllegalOwner
+        ))
+    );
+}
+
+#[tokio::test]
+async fn fail_request_after_end_date() {
+    let mut context = nft_packs_program_test().start_with_context().await;
+
+    let name = [7; 32];
+    let uri = String::from("some link to storage");
+    let description = String::from("Pack description");
+
+    let clock = context.banks_client.get_clock().await.unwrap();
+
+    let redeem_start_date = Some(clock.unix_timestamp as u64);
+    let redeem_end_date = Some(redeem_start_date.unwrap() + 100);
+
+    let store_admin = Keypair::new();
+    let store_key = create_store(&mut context, &store_admin, true)
+        .await
+        .unwrap();
+
+    let test_pack_set = TestPackSet::new(store_key);
+    test_pack_set
+        .init(
+            &mut context,
+            InitPackSetArgs {
+                name,
+                uri: uri.clone(),
+                description: description.clone(),
+                mutable: true,
+                distribution_type: PackDistributionType::Fixed,
+                allowed_amount_to_redeem: 10,
+                redeem_start_date,
+                redeem_end_date,
+            },
+        )
+        .await
+        .unwrap();
+
+    let (card_metadata, card_master_edition, card_master_token_holder) =
+        create_master_edition(&mut context, &test_pack_set).await;
+
+    let (card_metadata2, card_master_edition2, card_master_token_holder2) =
+        create_master_edition(&mut context, &test_pack_set).await;
+
+    let (voucher_metadata, voucher_master_edition, voucher_master_token_holder) =
+        create_master_edition(&mut context, &test_pack_set).await;
+
+    let voucher_edition = TestEditionMarker::new(&voucher_metadata, &voucher_master_edition, 1);
+    let edition_authority = Keypair::new();
+
+    let tx = Transaction::new_signed_with_payer(
+        &[system_instruction::create_account(
+            &context.payer.pubkey(),
+            &edition_authority.pubkey(),
+            100000000000000,
+            0,
+            &solana_program::system_program::id(),
+        )],
+        Some(&context.payer.pubkey()),
+        &[&context.payer, &edition_authority],
+        context.last_blockhash,
+    );
+
+    context.banks_client.process_transaction(tx).await.unwrap();
+
+    voucher_edition
+        .create(
+            &mut context,
+            &edition_authority,
+            &test_pack_set.authority,
+            &voucher_master_token_holder.token_account,
+        )
+        .await
+        .unwrap();
+
+    let test_pack_card = TestPackCard::new(&test_pack_set, 1);
+    test_pack_set
+        .add_card(
+            &mut context,
+            &test_pack_card,
+            &card_master_edition,
+            &card_metadata,
+            &card_master_token_holder,
+            AddCardToPackArgs {
+                max_supply: 5,
+                weight: 100,
+                index: test_pack_card.index,
+            },
+        )
+        .await
+        .unwrap();
+
+    let test_pack_card2 = TestPackCard::new(&test_pack_set, 2);
+    test_pack_set
+        .add_card(
+            &mut context,
+            &test_pack_card2,
+            &card_master_edition2,
+            &card_metadata2,
+            &card_master_token_holder2,
+            AddCardToPackArgs {
+                max_supply: 5,
+                weight: 100,
+                index: test_pack_card2.index,
+            },
+        )
+        .await
+        .unwrap();
+
+    let test_pack_voucher = TestPackVoucher::new(&test_pack_set, 1);
+    test_pack_set
+        .add_voucher(
+            &mut context,
+            &test_pack_voucher,
+            &voucher_master_edition,
+            &voucher_metadata,
+            &voucher_master_token_holder,
+        )
+        .await
+        .unwrap();
+
+    test_pack_set.activate(&mut context).await.unwrap();
+    test_pack_set.clean_up(&mut context).await.unwrap();
+
+    let mut test_randomness_oracle = TestRandomnessOracle::new();
+    test_randomness_oracle.init(&mut context).await.unwrap();
+    test_randomness_oracle.update(&mut context).await.unwrap();
+
+    // Wait until we reach over `redeem_end_date` timestamp
+    warp_sleep(&mut context, std::time::Duration::from_secs(5)).await;
+    let last_timestamp = context
+        .banks_client
+        .get_clock()
+        .await
+        .unwrap()
+        .unix_timestamp as u64;
+    assert_eq!(last_timestamp > redeem_end_date.unwrap(), true);
+
+    let err = test_pack_set
+        .request_card_for_redeem(
+            &mut context,
+            &store_key,
+            &voucher_edition.new_edition_pubkey,
+            &voucher_edition.mint.pubkey(),
+            &edition_authority,
+            &voucher_edition.token.pubkey(),
+            &test_randomness_oracle.keypair.pubkey(),
+            1,
+        )
+        .await
+        .unwrap_err();
+
+    assert_custom_error!(err, NFTPacksError::WrongRedeemDate, 0);
+}
+
+#[tokio::test]
+async fn fail_request_with_invalid_voucher() {
+    let mut context = nft_packs_program_test().start_with_context().await;
+
+    let name = [7; 32];
+    let uri = String::from("some link to storage");
+    let description = String::from("Pack description");
+
+    let clock = context.banks_client.get_clock().await.unwrap();
+
+    let redeem_start_date = Some(clock.unix_timestamp as u64);
+    let redeem_end_date = Some(redeem_start_date.unwrap() + 100);
+
+    let store_admin = Keypair::new();
+    let store_key = create_store(&mut context, &store_admin, true)
+        .await
+        .unwrap();
+
+    let test_pack_set = TestPackSet::new(store_key);
+    test_pack_set
+        .init(
+            &mut context,
+            InitPackSetArgs {
+                name,
+                uri: uri.clone(),
+                description: description.clone(),
+                mutable: true,
+                distribution_type: PackDistributionType::Fixed,
+                allowed_amount_to_redeem: 10,
+                redeem_start_date,
+                redeem_end_date,
+            },
+        )
+        .await
+        .unwrap();
+
+    let (card_metadata, card_master_edition, card_master_token_holder) =
+        create_master_edition(&mut context, &test_pack_set).await;
+
+    let (card_metadata2, card_master_edition2, card_master_token_holder2) =
+        create_master_edition(&mut context, &test_pack_set).await;
+
+    let (voucher_metadata, voucher_master_edition, voucher_master_token_holder) =
+        create_master_edition(&mut context, &test_pack_set).await;
+
+    let voucher_edition = TestEditionMarker::new(&voucher_metadata, &voucher_master_edition, 1);
+    let edition_authority = Keypair::new();
+
+    let tx = Transaction::new_signed_with_payer(
+        &[system_instruction::create_account(
+            &context.payer.pubkey(),
+            &edition_authority.pubkey(),
+            100000000000000,
+            0,
+            &solana_program::system_program::id(),
+        )],
+        Some(&context.payer.pubkey()),
+        &[&context.payer, &edition_authority],
+        context.last_blockhash,
+    );
+
+    context.banks_client.process_transaction(tx).await.unwrap();
+
+    voucher_edition
+        .create(
+            &mut context,
+            &edition_authority,
+            &test_pack_set.authority,
+            &voucher_master_token_holder.token_account,
+        )
+        .await
+        .unwrap();
+
+    let test_pack_card = TestPackCard::new(&test_pack_set, 1);
+    test_pack_set
+        .add_card(
+            &mut context,
+            &test_pack_card,
+            &card_master_edition,
+            &card_metadata,
+            &card_master_token_holder,
+            AddCardToPackArgs {
+                max_supply: 5,
+                weight: 100,
+                index: test_pack_card.index,
+            },
+        )
+        .await
+        .unwrap();
+
+    let test_pack_card2 = TestPackCard::new(&test_pack_set, 2);
+    test_pack_set
+        .add_card(
+            &mut context,
+            &test_pack_card2,
+            &card_master_edition2,
+            &card_metadata2,
+            &card_master_token_holder2,
+            AddCardToPackArgs {
+                max_supply: 5,
+                weight: 100,
+                index: test_pack_card2.index,
+            },
+        )
+        .await
+        .unwrap();
+
+    let test_pack_voucher = TestPackVoucher::new(&test_pack_set, 1);
+    test_pack_set
+        .add_voucher(
+            &mut context,
+            &test_pack_voucher,
+            &voucher_master_edition,
+            &voucher_metadata,
+            &voucher_master_token_holder,
+        )
+        .await
+        .unwrap();
+
+    test_pack_set.activate(&mut context).await.unwrap();
+    test_pack_set.clean_up(&mut context).await.unwrap();
+
+    let mut test_randomness_oracle = TestRandomnessOracle::new();
+    test_randomness_oracle.init(&mut context).await.unwrap();
+    test_randomness_oracle.update(&mut context).await.unwrap();
+
+    // Wait until we reach over `redeem_end_date` timestamp
+    warp_sleep(&mut context, std::time::Duration::from_secs(5)).await;
+    let last_timestamp = context
+        .banks_client
+        .get_clock()
+        .await
+        .unwrap()
+        .unix_timestamp as u64;
+    assert_eq!(last_timestamp > redeem_end_date.unwrap(), true);
+
+    let err = test_pack_set
+        .request_card_for_redeem_fake_voucher(
+            &mut context,
+            &store_key,
+            &voucher_edition.new_edition_pubkey,
+            &voucher_edition.mint.pubkey(),
+            &edition_authority,
+            &voucher_edition.token.pubkey(),
+            &test_randomness_oracle.keypair.pubkey(),
+            1,
+        )
+        .await
+        .unwrap_err();
+
+    assert_transport_error!(
+        err,
         TransportError::TransactionError(TransactionError::InstructionError(
             0,
             InstructionError::IllegalOwner
