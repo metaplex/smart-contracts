fce966541a33ef2f19667780623951373ed92e2d
bezrazli4n0 <rpglovemmo@gmail.com>
2021-11-23 19:37:39 +0300

    feature: added decrement cards positive test in ClaimPack instruction, added positive test for ensure, that card can be clamed two times, fixed twice test

--- metaplex-tests/tests/claim_card.rs
+++ metaplex-tests/tests/claim_card.rs
@@ -388,6 +388,405 @@ async fn success_max_supply_probability() {
 }
 
 #[tokio::test]
+async fn success_claim_two_same_cards() {
+    let mut context = nft_packs_program_test().start_with_context().await;
+
+    let name = [7; 32];
+    let uri = String::from("some link to storage");
+    let description = String::from("Pack description");
+
+    let clock = context.banks_client.get_clock().await.unwrap();
+
+    let redeem_start_date = Some(clock.unix_timestamp as u64);
+    let redeem_end_date = Some(redeem_start_date.unwrap() + 100);
+
+    let store_admin = Keypair::new();
+    let store_key = create_store(&mut context, &store_admin, true)
+        .await
+        .unwrap();
+
+    let test_pack_set = TestPackSet::new(store_key);
+    test_pack_set
+        .init(
+            &mut context,
+            InitPackSetArgs {
+                name,
+                uri: uri.clone(),
+                description: description.clone(),
+                mutable: true,
+                distribution_type: PackDistributionType::Fixed,
+                allowed_amount_to_redeem: 2,
+                redeem_start_date,
+                redeem_end_date,
+            },
+        )
+        .await
+        .unwrap();
+
+    // Create first card master edition
+    let (card_metadata, card_master_edition, card_master_token_holder) =
+        create_master_edition(&mut context, &test_pack_set).await;
+
+    let (voucher_metadata, voucher_master_edition, voucher_master_token_holder) =
+        create_master_edition(&mut context, &test_pack_set).await;
+
+    let voucher_edition = TestEditionMarker::new(&voucher_metadata, &voucher_master_edition, 1);
+    let edition_authority = Keypair::new();
+
+    let tx = Transaction::new_signed_with_payer(
+        &[system_instruction::create_account(
+            &context.payer.pubkey(),
+            &edition_authority.pubkey(),
+            100000000000000,
+            0,
+            &solana_program::system_program::id(),
+        )],
+        Some(&context.payer.pubkey()),
+        &[&context.payer, &edition_authority],
+        context.last_blockhash,
+    );
+
+    context.banks_client.process_transaction(tx).await.unwrap();
+
+    voucher_edition
+        .create(
+            &mut context,
+            &edition_authority,
+            &test_pack_set.authority,
+            &voucher_master_token_holder.token_account,
+        )
+        .await
+        .unwrap();
+
+    // Add first card
+    let test_pack_card = TestPackCard::new(&test_pack_set, 1);
+    let card_max_supply = 2;
+    let card_weight = 100;
+    test_pack_set
+        .add_card(
+            &mut context,
+            &test_pack_card,
+            &card_master_edition,
+            &card_metadata,
+            &card_master_token_holder,
+            AddCardToPackArgs {
+                max_supply: card_max_supply,
+                weight: card_weight,
+                index: test_pack_card.index,
+            },
+        )
+        .await
+        .unwrap();
+
+    // Add voucher to PackSet
+    let test_pack_voucher = TestPackVoucher::new(&test_pack_set, 1);
+    test_pack_set
+        .add_voucher(
+            &mut context,
+            &test_pack_voucher,
+            &voucher_master_edition,
+            &voucher_metadata,
+            &voucher_master_token_holder,
+        )
+        .await
+        .unwrap();
+
+    test_pack_set.activate(&mut context).await.unwrap();
+    test_pack_set.clean_up(&mut context).await.unwrap();
+
+    let new_mint = Keypair::new();
+    let new_mint_token_acc = Keypair::new();
+
+    let new_mint1 = Keypair::new();
+    let new_mint_token_acc1 = Keypair::new();
+
+    let mut test_randomness_oracle = TestRandomnessOracle::new();
+    test_randomness_oracle.init(&mut context).await.unwrap();
+    test_randomness_oracle.update(&mut context).await.unwrap();
+
+    test_pack_set
+        .request_card_for_redeem(
+            &mut context,
+            &store_key,
+            &voucher_edition.new_edition_pubkey,
+            &voucher_edition.mint.pubkey(),
+            &edition_authority,
+            &Some(voucher_edition.token.pubkey()),
+            &test_randomness_oracle.keypair.pubkey(),
+            1,
+        )
+        .await
+        .unwrap();
+
+    context.warp_to_slot(5).unwrap();
+    test_pack_set.clean_up(&mut context).await.unwrap();
+
+    test_pack_set
+        .request_card_for_redeem(
+            &mut context,
+            &store_key,
+            &voucher_edition.new_edition_pubkey,
+            &voucher_edition.mint.pubkey(),
+            &edition_authority,
+            &None,
+            &test_randomness_oracle.keypair.pubkey(),
+            1,
+        )
+        .await
+        .unwrap();
+
+    context.warp_to_slot(7).unwrap();
+
+    test_pack_set
+        .claim_pack(
+            &mut context,
+            &edition_authority,
+            &voucher_edition.mint.pubkey(),
+            &test_pack_card.token_account.pubkey(),
+            &card_master_edition.pubkey,
+            &new_mint,
+            &new_mint_token_acc,
+            &edition_authority,
+            &card_metadata.pubkey,
+            &card_master_edition.mint_pubkey,
+            &test_randomness_oracle.keypair.pubkey(),
+            1,
+        )
+        .await
+        .unwrap();
+
+    context.warp_to_slot(9).unwrap();
+
+    test_pack_set
+        .claim_pack(
+            &mut context,
+            &edition_authority,
+            &voucher_edition.mint.pubkey(),
+            &test_pack_card.token_account.pubkey(),
+            &card_master_edition.pubkey,
+            &new_mint1,
+            &new_mint_token_acc1,
+            &edition_authority,
+            &card_metadata.pubkey,
+            &card_master_edition.mint_pubkey,
+            &test_randomness_oracle.keypair.pubkey(),
+            1,
+        )
+        .await
+        .unwrap();
+
+    let card_master_edition = card_master_edition.get_data(&mut context).await;
+    assert_eq!(card_master_edition.supply, 2);
+}
+
+#[tokio::test]
+async fn success_claim_decrement_redeem_cards() {
+    let mut context = nft_packs_program_test().start_with_context().await;
+
+    let name = [7; 32];
+    let uri = String::from("some link to storage");
+    let description = String::from("Pack description");
+
+    let clock = context.banks_client.get_clock().await.unwrap();
+
+    let redeem_start_date = Some(clock.unix_timestamp as u64);
+    let redeem_end_date = Some(redeem_start_date.unwrap() + 100);
+
+    let store_admin = Keypair::new();
+    let store_key = create_store(&mut context, &store_admin, true)
+        .await
+        .unwrap();
+
+    let test_pack_set = TestPackSet::new(store_key);
+    test_pack_set
+        .init(
+            &mut context,
+            InitPackSetArgs {
+                name,
+                uri: uri.clone(),
+                description: description.clone(),
+                mutable: true,
+                distribution_type: PackDistributionType::Fixed,
+                allowed_amount_to_redeem: 2,
+                redeem_start_date,
+                redeem_end_date,
+            },
+        )
+        .await
+        .unwrap();
+
+    // Create first card master edition
+    let (card_metadata, card_master_edition, card_master_token_holder) =
+        create_master_edition(&mut context, &test_pack_set).await;
+
+    let (voucher_metadata, voucher_master_edition, voucher_master_token_holder) =
+        create_master_edition(&mut context, &test_pack_set).await;
+
+    let voucher_edition = TestEditionMarker::new(&voucher_metadata, &voucher_master_edition, 1);
+    let edition_authority = Keypair::new();
+
+    let tx = Transaction::new_signed_with_payer(
+        &[system_instruction::create_account(
+            &context.payer.pubkey(),
+            &edition_authority.pubkey(),
+            100000000000000,
+            0,
+            &solana_program::system_program::id(),
+        )],
+        Some(&context.payer.pubkey()),
+        &[&context.payer, &edition_authority],
+        context.last_blockhash,
+    );
+
+    context.banks_client.process_transaction(tx).await.unwrap();
+
+    voucher_edition
+        .create(
+            &mut context,
+            &edition_authority,
+            &test_pack_set.authority,
+            &voucher_master_token_holder.token_account,
+        )
+        .await
+        .unwrap();
+
+    // Add first card
+    let test_pack_card = TestPackCard::new(&test_pack_set, 1);
+    let card_max_supply = 2;
+    let card_weight = 100;
+    test_pack_set
+        .add_card(
+            &mut context,
+            &test_pack_card,
+            &card_master_edition,
+            &card_metadata,
+            &card_master_token_holder,
+            AddCardToPackArgs {
+                max_supply: card_max_supply,
+                weight: card_weight,
+                index: test_pack_card.index,
+            },
+        )
+        .await
+        .unwrap();
+
+    // Add voucher to PackSet
+    let test_pack_voucher = TestPackVoucher::new(&test_pack_set, 1);
+    test_pack_set
+        .add_voucher(
+            &mut context,
+            &test_pack_voucher,
+            &voucher_master_edition,
+            &voucher_metadata,
+            &voucher_master_token_holder,
+        )
+        .await
+        .unwrap();
+
+    test_pack_set.activate(&mut context).await.unwrap();
+    test_pack_set.clean_up(&mut context).await.unwrap();
+
+    let new_mint = Keypair::new();
+    let new_mint_token_acc = Keypair::new();
+
+    let new_mint1 = Keypair::new();
+    let new_mint_token_acc1 = Keypair::new();
+
+    let mut test_randomness_oracle = TestRandomnessOracle::new();
+    test_randomness_oracle.init(&mut context).await.unwrap();
+    test_randomness_oracle.update(&mut context).await.unwrap();
+
+    test_pack_set
+        .request_card_for_redeem(
+            &mut context,
+            &store_key,
+            &voucher_edition.new_edition_pubkey,
+            &voucher_edition.mint.pubkey(),
+            &edition_authority,
+            &Some(voucher_edition.token.pubkey()),
+            &test_randomness_oracle.keypair.pubkey(),
+            1,
+        )
+        .await
+        .unwrap();
+
+    context.warp_to_slot(5).unwrap();
+    test_pack_set.clean_up(&mut context).await.unwrap();
+    let (proving_process_key, _) = find_proving_process_program_address(
+        &metaplex_nft_packs::id(),
+        &test_pack_set.keypair.pubkey(),
+        &edition_authority.pubkey(),
+        &voucher_edition.mint.pubkey(),
+    );
+
+    test_pack_set
+        .request_card_for_redeem(
+            &mut context,
+            &store_key,
+            &voucher_edition.new_edition_pubkey,
+            &voucher_edition.mint.pubkey(),
+            &edition_authority,
+            &None,
+            &test_randomness_oracle.keypair.pubkey(),
+            1,
+        )
+        .await
+        .unwrap();
+
+    context.warp_to_slot(7).unwrap();
+
+    let proving_process_data = get_account(&mut context, &proving_process_key).await;
+    let proving_process = ProvingProcess::unpack_from_slice(&proving_process_data.data).unwrap();
+    assert_eq!(*proving_process.cards_to_redeem.get(&1).unwrap(), 2);
+
+    test_pack_set
+        .claim_pack(
+            &mut context,
+            &edition_authority,
+            &voucher_edition.mint.pubkey(),
+            &test_pack_card.token_account.pubkey(),
+            &card_master_edition.pubkey,
+            &new_mint,
+            &new_mint_token_acc,
+            &edition_authority,
+            &card_metadata.pubkey,
+            &card_master_edition.mint_pubkey,
+            &test_randomness_oracle.keypair.pubkey(),
+            1,
+        )
+        .await
+        .unwrap();
+
+    context.warp_to_slot(9).unwrap();
+
+    let proving_process_data = get_account(&mut context, &proving_process_key).await;
+    let proving_process = ProvingProcess::unpack_from_slice(&proving_process_data.data).unwrap();
+    assert_eq!(*proving_process.cards_to_redeem.get(&1).unwrap(), 1);
+
+    test_pack_set
+        .claim_pack(
+            &mut context,
+            &edition_authority,
+            &voucher_edition.mint.pubkey(),
+            &test_pack_card.token_account.pubkey(),
+            &card_master_edition.pubkey,
+            &new_mint1,
+            &new_mint_token_acc1,
+            &edition_authority,
+            &card_metadata.pubkey,
+            &card_master_edition.mint_pubkey,
+            &test_randomness_oracle.keypair.pubkey(),
+            1,
+        )
+        .await
+        .unwrap();
+
+    let proving_process_data = get_account(&mut context, &proving_process_key).await;
+    let proving_process = ProvingProcess::unpack_from_slice(&proving_process_data.data).unwrap();
+    assert_eq!(*proving_process.cards_to_redeem.get(&1).unwrap(), 0);
+}
+
+#[tokio::test]
 async fn success_claim_two_indexes() {
     let mut context = nft_packs_program_test().start_with_context().await;
 
@@ -1194,5 +1593,5 @@ async fn fail_claim_twice() {
 
     let result = context.banks_client.process_transaction(tx).await;
 
-    assert_custom_error!(result.unwrap_err(), NFTPacksError::CardAlreadyRedeemed, 0);
+    assert_custom_error!(result.unwrap_err(), NFTPacksError::Underflow, 0);
 }
